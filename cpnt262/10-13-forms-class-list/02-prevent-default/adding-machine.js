b4eb90de4e126f9dee8744fd06c9fa5227dad9da
Tony Grimes <acidtone@tonygrimes.com>
2021-10-13 10:44:08 -0600

    refactored adding machine

--- cpnt262/10-13-forms-class-list/02-prevent-default/adding-machine.js
+++ cpnt262/10-13-forms-class-list/02-prevent-default/adding-machine.js
@@ -1,10 +1,14 @@
+// Create a variable for the form
 const form = document.querySelector('form');
 
 const handleSubmit = function(event) {
+  // Stop form from submitting and refreshing the page
   event.preventDefault();
 
+  // Calculate total
   const total = Number(form.first.value) + Number(form.second.value);
 
+  // Print total to page
   form.total.value = total;
   // console.log(event);
 }
