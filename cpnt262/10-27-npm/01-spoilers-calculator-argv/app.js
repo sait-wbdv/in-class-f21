7fa1349e9dd21660e8e4d113bccbd3c1fefdc294
Tony Grimes <acidtone@tonygrimes.com>
2021-10-27 09:19:33 -0600

    added resource links as commentws

--- cpnt262/10-27-npm/01-spoilers-calculator-argv/app.js
+++ cpnt262/10-27-npm/01-spoilers-calculator-argv/app.js
@@ -1,9 +1,15 @@
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
 const args = process.argv.slice(2)
 
+// See: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
 const num1 = Number(args[0])
 const operation = args[1]
 const num2 = Number(args[2])
 
+// See: 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Logical_OR
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/NaN
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
 if (isNaN(num1) || isNaN(num2)) {
 
   console.log('Please enter valid command.')
