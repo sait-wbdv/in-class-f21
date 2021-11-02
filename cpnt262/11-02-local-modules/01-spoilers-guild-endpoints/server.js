c289a0485da7c150febf779ad749ab913631cccd
Tony Grimes <acidtone@tonygrimes.com>
2021-11-02 09:35:36 -0600

    raw spoilers

---
+++ cpnt262/11-02-local-modules/01-spoilers-guild-endpoints/server.js
@@ -0,0 +1,88 @@
+// Install modules
+const express = require('express')
+const app = express()
+
+// Define seed data
+// Your data here
+const guild = [
+  // Item 1
+  {
+    // general
+    id: 1,
+    name: 'harry',
+    class: 'wizard',
+    race: 'elf',
+    // stats
+    strength: 16,
+    dexterity: 14,
+    constitution: 12,
+    intelligence: 14,
+    wisdom: 16,
+    charisma: 10,
+    // Modifiers
+    poisoned: false
+  },
+  // Item 2
+  {
+    // general
+    id: 2,
+    name: 'ryan',
+    class: 'bard',
+    race: 'human',
+    // stats
+    strength: 12,
+    dexterity: 16,
+    constitution: 14,
+    intelligence: 6,
+    wisdom: 10,
+    charisma: 20,
+    // Modifiers
+    poisoned: true
+  },
+  {
+    // general
+    id: 3,
+    name: 'spry',
+    class: 'rogue',
+    race: 'elf',
+    // stats
+    strength: 6,
+    dexterity: 18,
+    constitution: 10,
+    intelligence: 18,
+    wisdom: 14,
+    charisma: 8,
+    // Modifiers
+    poisoned: false
+  }
+];
+
+// Dynamic JSON Endpoint
+app.get('/api/guild', function(request, response) {
+  response.send(guild)
+})
+
+app.get('/api/guild/:name', function(request, response) {
+  
+  const character = guild.find(function(item){
+    if (request.params.name === item.name) {
+      return true
+    } else {
+      return false
+    }
+  }); // Use Array.find() here
+  response.send(character)
+})
+
+// Handle 404 errors with middleware
+app.use(function(request, response) {
+  response.status(404)
+  response.send('404: File Not Found')
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
