204614074ee82af684ec3f81f69a9eecebff1525
Tony Grimes <acidtone@tonygrimes.com>
2021-11-02 11:10:44 -0600

    TODO done: moved data to local module, cleaned up base cocde

--- cpnt262/11-02-local-modules/02-guild-arvg/app.js
+++ cpnt262/11-02-local-modules/02-guild-arvg/app.js
@@ -1,67 +1,10 @@
 // Accept command line input
-const inputValue = process.argv[2];
-
-// TODO: move array to local module
-// Define data array
-const guild = [
-  // Item 1
-  {
-    // general
-    id: 1,
-    name: 'Harry',
-    class: 'wizard',
-    race: 'elf',
-    // stats
-    strength: 16,
-    dexterity: 14,
-    constitution: 12,
-    intelligence: 14,
-    wisdom: 16,
-    charisma: 10,
-    // Modifiers
-    poisoned: false
-  },
-  // Item 2
-  {
-    // general
-    id: 2,
-    name: 'Ryan Reynolds',
-    class: 'bard',
-    race: 'human',
-    // stats
-    strength: 12,
-    dexterity: 16,
-    constitution: 14,
-    intelligence: 6,
-    wisdom: 10,
-    charisma: 20,
-    // Modifiers
-    poisoned: true
-  },
-  {
-    // general
-    id: 3,
-    name: 'Spry',
-    class: 'rogue',
-    race: 'elf',
-    // stats
-    strength: 6,
-    dexterity: 18,
-    constitution: 10,
-    intelligence: 18,
-    wisdom: 14,
-    charisma: 8,
-    // Modifiers
-    poisoned: false
-  }
-];
-
-// Your code here
-console.log(inputValue)
+const inputString = process.argv[2];
+const guild = require('./guild')
 
 // TODO: clean up our expression
 const matchingCharacter = guild.find(function(item) {
-  if (Number(inputValue) === item.wisdom) {
+  if (inputString === item.name) {
     return true;
   } else {
     return false;
@@ -70,7 +13,7 @@ const matchingCharacter = guild.find(function(item) {
 
 // Log the results
 if (matchingCharacter) {
-  console.log(`Found a guild member with ${inputValue} Wisdom: \n`, matchingCharacter);
+  console.log(`Found ${inputString}: \n`, matchingCharacter);
 } else {
-  console.log(`Couldn't find any guild members with ${inputValue} Wisdom. Please try again.`);
+  console.log(`Couldn't find ${inputString}. Please try again.`);
 }
