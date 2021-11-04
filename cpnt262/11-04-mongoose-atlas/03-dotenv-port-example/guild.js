48fa0152b1c068462da6dbff7df4e256efac8624
Tony Grimes <acidtone@tonygrimes.com>
2021-11-04 10:19:34 -0600

    initial dotenv commit

---
+++ cpnt262/11-04-mongoose-atlas/03-dotenv-port-example/guild.js
@@ -0,0 +1,58 @@
+/*********************/
+/* Define data array */
+/*********************/
+
+const guild = [
+  // Item 1
+  {
+    // general
+    id: 1,
+    name: 'harry',
+    class: 'wizard',
+    race: 'elf',
+    // stats
+    strength: 16,
+    dexterity: 14,
+    constitution: 12,
+    intelligence: 14,
+    wisdom: 16,
+    charisma: 10,
+    // Modifiers
+    poisoned: false
+  },
+  // Item 2
+  {
+    // general
+    id: 2,
+    name: 'ryan',
+    class: 'bard',
+    race: 'human',
+    // stats
+    strength: 12,
+    dexterity: 16,
+    constitution: 14,
+    intelligence: 6,
+    wisdom: 10,
+    charisma: 20,
+    // Modifiers
+    poisoned: true
+  },
+  {
+    // general
+    id: 3,
+    name: 'spry',
+    class: 'rogue',
+    race: 'elf',
+    // stats
+    strength: 6,
+    dexterity: 18,
+    constitution: 10,
+    intelligence: 18,
+    wisdom: 14,
+    charisma: 8,
+    // Modifiers
+    poisoned: false
+  }
+]
+
+module.exports = guild
---
+++ cpnt262/11-04-mongoose-atlas/03-dotenv-port-example/server.js
@@ -0,0 +1,88 @@
+/******************/
+/* Import Modules */
+/******************/
+const express = require('express')
+const app = express()
+
+const guild = require('./guild')
+const randomItem = require('./random-item')
+
+/*****************/
+/* Define routes */
+/*****************/
+
+// List entry route
+app.get('/api/guild', (req, res) => {
+  let randomCharacter = null; // for Random member filter
+  let poisonedCharacter = null; // for Poisoned member filter
+
+  if (req.query.filter === 'random') {   
+
+    randomCharacter = randomItem(guild)
+    res.send(randomCharacter)
+
+  } else if (req.query.filter === 'poisoned') {
+
+    poisonedCharacter = guild.filter(item => item.poisoned)
+    res.send(poisonedCharacter)
+
+  } else if (typeof guild !== 'undefined' && Array.isArray(guild)) {
+
+    // Variable is an array!
+    res.send(guild)
+
+  } else {
+
+    res.status(404)
+    res.send({error: 'File Not Found'})
+    
+  }
+
+})
+
+// Item route
+app.get('/api/guild/:name', (req, res) => {
+  let character
+
+  if (typeof guild !== 'undefined' && Array.isArray(guild)) {
+    character = guild.find(item => req.params.name === item.name) // Use Array.find() here
+  } else {
+    character = null;
+  }
+  
+  if (typeof character === 'object' && character !== null) {
+    res.send(character)
+  } else {
+    res.status(404)
+    res.send({error: 'File Not Found'})
+  }
+})
+
+/****************************/
+/* Handle 404, start server */
+/****************************/
+
+// Handle 404 errors with middleware
+app.use((req, res) => {
+
+  // If path starts with `/api`, send JSON 404
+  if (req.url.startsWith('/api')) {
+
+    res.status(404)
+    res.send({error: 'File Not Found'})
+
+  } else {
+  
+    // else send HTML 404
+    res.status(404)
+    res.send('<h1>404: File Not Found</h1>')
+
+  }
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
