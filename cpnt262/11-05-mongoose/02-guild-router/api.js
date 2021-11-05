0a8f57b2c585f0451cb40464a792aca7c8033d11
Tony Grimes <acidtone@tonygrimes.com>
2021-11-05 09:51:09 -0600

    moved api handlers to their own router

---
+++ cpnt262/11-05-mongoose/02-guild-router/api.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const router = express.Router()
+
+/****************************/
+/* Include API dependencies */
+/****************************/
+
+const guild = require('./guild')
+const randomItem = require('./random-item')
+
+/*********************/
+/* Define API routes */
+/*********************/
+
+// List entry route
+router.get('/guild', (req, res) => {
+  let randomCharacter = null; // for Random member filter
+  let poisonedCharacter = null; // for Poisoned member filter
+
+  if (req.query.filter === 'random') {   
+
+    randomCharacter = randomItem(guild)
+    res.send(randomCharacter)
+
+  } else if (req.query.filter === 'poisoned') {
+
+    poisonedCharacter = guild.filter(item => item.poisoned)
+    res.send(poisonedCharacter)
+
+  } else if (typeof guild !== 'undefined' && Array.isArray(guild)) {
+
+    // Variable is an array!
+    res.send(guild)
+    
+  } else {
+    
+    console.log(guild)
+
+    res.status(404)
+    res.send({error: 'File Not Fooound'})
+    
+  }
+
+})
+
+// Item route
+router.get('/guild/:name', (req, res) => {
+  let character
+
+  if (typeof guild !== 'undefined' && Array.isArray(guild)) {
+    character = guild.find(item => req.params.name === item.name) // Use Array.find() here
+  } else {
+    character = null;
+  }
+  
+  if (typeof character === 'object' && character !== null) {
+    res.send(character)
+  } else {
+    res.status(404)
+    res.send({error: 'File Not Found'})
+  }
+})
+
+module.exports = router
--- cpnt262/11-05-mongoose/02-guild-router/server.js
+++ cpnt262/11-05-mongoose/02-guild-router/server.js
@@ -5,59 +5,12 @@ const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
 
-const guild = require('./guild')
-const randomItem = require('./random-item')
-
 /*****************/
 /* Define routes */
 /*****************/
+const api = require('./api.js')
+app.use('/api', api)
 
-// List entry route
-app.get('/api/guild', (req, res) => {
-  let randomCharacter = null; // for Random member filter
-  let poisonedCharacter = null; // for Poisoned member filter
-
-  if (req.query.filter === 'random') {   
-
-    randomCharacter = randomItem(guild)
-    res.send(randomCharacter)
-
-  } else if (req.query.filter === 'poisoned') {
-
-    poisonedCharacter = guild.filter(item => item.poisoned)
-    res.send(poisonedCharacter)
-
-  } else if (typeof guild !== 'undefined' && Array.isArray(guild)) {
-
-    // Variable is an array!
-    res.send(guild)
-
-  } else {
-
-    res.status(404)
-    res.send({error: 'File Not Found'})
-    
-  }
-
-})
-
-// Item route
-app.get('/api/guild/:name', (req, res) => {
-  let character
-
-  if (typeof guild !== 'undefined' && Array.isArray(guild)) {
-    character = guild.find(item => req.params.name === item.name) // Use Array.find() here
-  } else {
-    character = null;
-  }
-  
-  if (typeof character === 'object' && character !== null) {
-    res.send(character)
-  } else {
-    res.status(404)
-    res.send({error: 'File Not Found'})
-  }
-})
 
 /****************************/
 /* Handle 404, start server */
