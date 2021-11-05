0982140edf09499da423fb53fff8b35cb21418cb
Tony Grimes <acidtone@tonygrimes.com>
2021-11-05 10:51:37 -0600

    cleanup

--- cpnt262/11-05-mongoose/03-starter-kittens/server.js
+++ cpnt262/11-05-mongoose/03-starter-kittens/server.js
@@ -12,3 +12,22 @@ mongoose.connect(
   .catch(function(err){
     console.log(err)
   });
+
+const kittySchema = new mongoose.Schema({
+  name: String,
+  legs: Number
+});
+
+const Kitten = mongoose.model('Kitten', kittySchema)
+
+const silence = new Kitten({ name: 'Silence', legs: 4 })
+
+const fluffy = new Kitten({ name: 'fluffy', legs: 4 })
+
+const tripod = new Kitten({ name: 'Tripod', legs: 3 })
+
+console.log(silence, fluffy, tripod)
+
+silence.save()
+fluffy.save()
+tripod.save()
