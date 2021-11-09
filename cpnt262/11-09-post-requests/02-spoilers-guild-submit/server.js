e18be674281772434470247f1fe82e9d9c01457b
Tony Grimes <acidtone@tonygrimes.com>
2021-11-09 10:48:57 -0700

    Added a validation TODO

--- cpnt262/11-09-post-requests/02-spoilers-guild-submit/server.js
+++ cpnt262/11-09-post-requests/02-spoilers-guild-submit/server.js
@@ -87,6 +87,7 @@ app.get('/api/guild/:name', async (request, response) => {
 
 app.post('/api/guild', async (request, response) => {
   // TODO: Add some validation with a try/catch block
+  // TODO: redirect to static response pages depending on try/catch result
   const player = new Player(request.body)
 
   await player.save()
